977138c0fa9c5e0fd1438814685a2a63625c7cd1
UJ DEV <uj@ujdev.com>
2022-09-06 22:15:41 +0100

    err commit

--- src/Error.tsx
+++ src/Error.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Error = () => {
   return (
-    <div>Error 404 </div>
+    <section className="error"> 
+      <h1> Error 404!!! You are at the wrong page </h1>
+      <Link to="/" className="backHome">
+          BACK HOME
+      </Link>
+    </section>
   )
 }
 
